d8393da3acabba37db93f1b31d4426efe1391ea9
satoyuhi-disc <bunkoro1220@gmail.com>
2025-07-01 11:04:54 +0900

    Greetings added

--- Dice.c
+++ Dice.c
@@ -4,9 +4,12 @@
 
 int main() {
     srand(time(NULL)); 
+    char name[100];
     int die1 = rand() % 6 + 1; 
     int die2 = rand() % 6 + 1;
-
+    printf("What is your name?\n");
+    fgets(name, sizeof(name),stdin);
+    printf("Hello, %s!\n", name);
     printf("Rolling the dice...\n");
     printf("Die 1: %d\n", die1);
     printf("Die 2: %d\n", die2);
