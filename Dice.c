6922a2abc8c6ffa9874fcdc1082308b41d048c5e
satoyuhi-disc <bunkoro1220@gmail.com>
2025-07-01 11:07:39 +0900

    Won/lost message added

--- Dice.c
+++ Dice.c
@@ -11,6 +11,10 @@ int main() {
     printf("Die 1: %d\n", die1);
     printf("Die 2: %d\n", die2);
     printf("Total value: %d\n", die1 + die2);
+    if(die1+die2>7) {
+        printf("You won!\n");
+    }
+    else printf("You lost!\n");
 
     return 0;
 }
